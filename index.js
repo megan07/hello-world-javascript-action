0aefc60f03a26baa02ea4bf6416273d994236233
Megan Bang <mbang@hashicorp.com>
2021-06-29 14:12:24 -0500

    test 1

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const random = require('random');
+const yaml = require('js-yaml');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const team = core.getInput('team');
+
+  let data = fs.readFileSync('../users.yml', 'utf8')
+  let users = yaml.safeLoad(data);
+  
+  let max = users["users"][team]["members"].length - 1
+  let randomNum = random.int(max=max)
+
+  let user = users["users"][team]["members"][randomNum]
+  console.log(`Hello ${user}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
