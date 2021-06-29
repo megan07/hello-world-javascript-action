785eaad462169babdb718556ee9c8b4dbdfbed6c
Megan Bang <mbang@hashicorp.com>
2021-06-29 15:59:33 -0500

    update to pull_request_target

--- index.js
+++ index.js
@@ -7,18 +7,15 @@ const yaml = require('js-yaml');
 try {
   const team = core.getInput('team');
 
-  let data = fs.readFileSync('.github/users.yml', 'utf8')
-  let users = yaml.safeLoad(data);
-
-  console.log(users, users["users"][team]["members"].length)
-  
-  let max = users["users"][team]["members"].length - 1
-  let randomNum = random.int(0, max)
-
-  console.log(randomNum, max)
+  let users = yaml.safeLoad(fs.readFileSync('.github/users.yml', 'utf8'));
+  let randomNum = random.int(0, users["users"][team]["members"].length - 1)
 
   let user = users["users"][team]["members"][randomNum]
-  console.log(`Hello ${team} ${user}!`);
+  console.log(`Hello ${user}!`);
+
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 } catch (error) {
