bcec8f5f96def33ef65691d80f3fcde1aea4864b
Megan Bang <mbang@hashicorp.com>
2021-06-29 14:48:12 -0500

    log things

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ try {
 
   let data = fs.readFileSync('.github/users.yml', 'utf8')
   let users = yaml.safeLoad(data);
+
+  console.log(data)
   
   let max = users["users"][team]["members"].length - 1
   let randomNum = random.int(max=max)
 
+  console.log(randomNum, max)
+
   let user = users["users"][team]["members"][randomNum]
-  console.log(`Hello ${user}!`);
+  console.log(`Hello ${team} ${user}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 } catch (error) {
