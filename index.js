8dde85149f042732a8e285b7c6a6a319cd73f88d
Megan Bang <mbang@hashicorp.com>
2022-06-28 13:41:48 -0500

    My first action is ready

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const core = require('@actions/core');
-const fs = require('fs');
 const github = require('@actions/github');
-const random = require('random');
-const yaml = require('js-yaml');
 
 try {
-  const team = core.getInput('team');
-
-  let users = yaml.safeLoad(fs.readFileSync('.github/users.yml', 'utf8'));
-  let randomNum = random.int(0, users["users"][team]["members"].length - 1)
-
-  let user = users["users"][team]["members"][randomNum]
-  console.log(`Hello ${user}!`);
-
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${github.context.payload._links.self.href}`);
-
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
