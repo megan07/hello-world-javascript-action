e78f61761dec89c22b8edaa7aaf080a2c2be3d26
Megan Bang <mbang@hashicorp.com>
2022-06-28 16:11:47 -0500

    keep working on it

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-import * as Webhooks from '@octokit/webhooks'
+const webhooks = require('@octokit/webhooks');
 
 try {
     // The pull_request exists on payload when a pull_request event is triggered.
     // Sets action status to failed when pull_request does not exist on payload.
-  //   const wh = github.context.payload as Webhooks
-  //   const pr = wh.pull_request as Webhooks.WebhookPayloadPullRequest
-  //   if (!pr) {
-  //     core.setFailed('github.context.payload.pull_request not exist')
-  //     return
-  //   }
-  // console.log(`The event pull request: ${payload.pull_request}`);
+    const pr = github.context.payload.pull_request
+    if (!pr) {
+      core.setFailed('github.context.payload.pull_request not exist')
+      throw new Error("pull request did not exist in payload")
+    }
+  console.log(`The event pull request: ${payload.pull_request}`);
   // Get input parameters.
   const token = core.getInput('repo-token')
 
@@ -32,9 +31,7 @@ try {
     body: message
   })
   core.debug(`created comment URL: ${response.data.html_url}`)
-
   core.setOutput('comment-url', response.data.html_url)
-
 } catch (error) {
   core.setFailed(error.message);
 }
